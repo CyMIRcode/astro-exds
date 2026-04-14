434f54d7af54d45fcc13f29d9dd0577e3467a17b
chris <148228792+CyMIRcode@users.noreply.github.com>
2026-04-14 15:51:15 +0800

    Update site.config.mjs

--- site.config.mjs
+++ site.config.mjs
@@ -11,11 +11,11 @@ if (!hasSiteUrl && process.env.NODE_ENV === 'production') {
 
 export const site = {
   url: hasSiteUrl ? siteUrl : fallbackSiteUrl,
-  title: 'Astro Themes by Whono',
-  brandTitle: 'Whono',
-  author: 'Whono',
+  title: 'Explore Dreams Daringly and Successfully.',
+  brandTitle: 'EXDS',
+  author: 'EXDS',
   authorAvatar: 'author/avatar.webp',
-  description: '一个 Astro 主题的展示站：轻量、可维护、可复用。'
+  description: 'Explore Dreams Daringly and Successfully.'
 };
 
 export const PAGE_SIZE_ARCHIVE = 12;
